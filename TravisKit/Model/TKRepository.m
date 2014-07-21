149b6ca8b64bdfbe8a157b9dcf8552864290a006
Dal Rupnik <legoless@gmail.com>
2014-07-21 21:30:47 +0200

    Update TKRepository.m

--- TravisKit/Model/TKRepository.m
+++ TravisKit/Model/TKRepository.m
@@ -1,13 +1,23 @@
 //
 //  TKRepository.m
-//  Pods
+//  TravisKit
 //
 //  Created by Dal Rupnik on 17/06/14.
-//
+//  Copyright (c) 2014 arvystate.net. All rights reserved.
 //
 
 #import "TKRepository.h"
 
 @implementation TKRepository
+
++ (BOOL)propertyIsOptional:(NSString *)propertyName
+{
+    if ([propertyName hasPrefix:@"last_build"])
+    {
+        return YES;
+    }
+    
+    return NO;
+}
 
 @end
