afaaa4a4ceaf7b5f7a33ae402322d10187abe26f
Dal Rupnik <legoless@gmail.com>
2014-09-11 12:57:12 +0200

    Update TKRepository.h

--- TravisKit/Model/TKRepository.h
+++ TravisKit/Model/TKRepository.h
@@ -14,11 +14,11 @@
 @property (nonatomic, strong) NSString* slug;
 @property (nonatomic, strong) NSString* description;
 @property (nonatomic) NSInteger last_build_id;
-@property (nonatomic, strong) NSString* last_build_number;
-@property (nonatomic, strong) NSString* last_build_state;
-@property (nonatomic, strong) NSNumber* last_build_duration;
-@property (nonatomic, strong) NSDate* last_build_started_at;
-@property (nonatomic, strong) NSDate* last_build_finished_at;
+@property (nonatomic, strong) NSString<Optional>* last_build_number;
+@property (nonatomic, strong) NSString<Optional>* last_build_state;
+@property (nonatomic, strong) NSNumber<Optional>* last_build_duration;
+@property (nonatomic, strong) NSDate<Optional>* last_build_started_at;
+@property (nonatomic, strong) NSDate<Optional>* last_build_finished_at;
 @property (nonatomic, strong) NSString<Optional>* github_language;
 
 @end
